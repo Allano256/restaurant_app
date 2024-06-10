b4a5c31b04b3094fb189c34014eb1788506f7d4a
Allan Zizinga <allanzizinga@yahoo.com>
2024-06-10 16:19:45 +0000

    Create initial model

--- restaurant_site/models.py
+++ restaurant_site/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length = 200, unique=True)
+    last_name = models.CharField(max_length = 200, unique=True)
+    customer_email = models.CharField(max_length = 200, unique=True)
+    number_of_tables = models.IntegerField(default=0)
+    number_of_guests = models.IntegerField(default=0)
+    message_to_restaurant = models.TextField()
+    booked_on = models.DateTimeField(auto_now_add=True)
+  
+
