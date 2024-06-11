98aac19932bd588b286ab7da7759517d1a11aefe
Allan Zizinga <allanzizinga@yahoo.com>
2024-06-11 22:16:38 +0000

    Added all fields in Admin display of booking

--- booking/admin.py
+++ booking/admin.py
@@ -4,6 +4,6 @@ from .models import Booking
 
 # Register your models here.
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ("name","number_of_guests" )
+    list_display = ("name","number_of_guests", "date", "time" )  
 
 admin.site.register(Booking, BookingAdmin)
