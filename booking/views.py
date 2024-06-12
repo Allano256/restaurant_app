6680b0ff554e4040a269df53c8cc64f4790a8234
Allan Zizinga <allanzizinga@yahoo.com>
2024-06-12 00:05:25 +0000

    Add user input functionality to book_table

--- booking/views.py
+++ booking/views.py
@@ -6,6 +6,16 @@ from .models import Booking
 
 # Create your views here.
 def book_table(request):
+    while (True):
+        table_request = input("Reservation y/n ?")
+        number_people = int(input("How many people are you reserving for?"))
+        response = table_request.capitalize()
+
+
+
+   
     return render(request, "booking/booking.html")
+
+
 
  
