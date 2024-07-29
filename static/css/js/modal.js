6ad52be8ce78aaf0db0da0a01538cc71a2e3cabb
Allan Zizinga <allanzizinga@yahoo.com>
2024-07-29 15:09:49 +0000

    Fixed an overlay on modal

--- static/css/js/modal.js
+++ static/css/js/modal.js
@@ -5,11 +5,13 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelector('.show-modal');
 const cancelConfirm = document.getElementById("cancelConfirm");
 const deleteButtons = document.getElementsByClassName("btn-delete");
+const overlay = document.querySelector('.overlay');
 
 
 const openModal = function (){
   
         modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
       
 }
 
@@ -17,9 +19,23 @@ btnOpenModal.addEventListener('click', openModal
 )
 
 const closeModal = function(){
-    modal.classList.add('hidden');   
+    modal.classList.add('hidden'); 
+    overlay.classList.add('hidden'); 
 }
+
 btnCloseModal.addEventListener('click', closeModal );
+
+btnCloseModal.addEventListener('click',closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' &&  !modal.classList.contains('hidden')) 
+    {
+      closeModal();
+    }
+ 
+})
 
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
