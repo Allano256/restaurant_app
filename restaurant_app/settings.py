ef196692fed572b9918a2b4077adadedcae839dc
Allan Zizinga <allanzizinga@yahoo.com>
2024-11-14 20:52:46 +0000

    Changed postgres database

--- restaurant_app/settings.py
+++ restaurant_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = [ '8000-allano256-restaurantapp-t3zrmicll2j.ws.codeinstitute-ide.net'  ,'.herokuapp.com' ]
 
